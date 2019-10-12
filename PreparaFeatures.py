570177c36c85edf543205bece8a1c49205be6644
Carlos Martinho <martinhoc@MartinhoBook.local>
2019-10-12 18:29:43 -0300

    csv files

--- PreparaFeatures.py
+++ PreparaFeatures.py
@@ -1,6 +1,15 @@
-import numpy as np
-#from statistics import mean
-import matplotlib.mlab as mlab
-import matplotlib.pyplot as plt
 
-print("oi galera");
+#import numpy as np
+#from statistics import mean
+#import matplotlib.mlab as mlab
+#import matplotlib.pyplot as plt
+
+
+
+print("oi galera")
+
+myfile = open('BVSP.xlsx', 'r')
+for line in myfile:
+    print(line)
+
+
